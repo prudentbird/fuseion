9d176de84c9ce84631612faa80ab7b69576f72d0
prudentbird <prudentbird@gmail.com>
2025-08-16 03:38:09 +0100

    feat: add @vercel/speed-insights for performance monitoring
    - Included @vercel/speed-insights dependency in package.json and pnpm-lock.yaml for enhanced performance insights.
    - Integrated SpeedInsights component in the Providers to monitor application performance effectively.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,8 +5,10 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "./theme";
 import { Toaster } from "~/components/ui/sonner";
 import { SessionProvider } from "next-auth/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 // import { VersionManager } from "~/components/ui/version-manager";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+
 const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL);
 
 const Providers = ({ children }: { children: ReactNode }) => {
@@ -21,6 +23,7 @@ const Providers = ({ children }: { children: ReactNode }) => {
         <ConvexProvider client={convex}>
           {children}
           <Toaster />
+          <SpeedInsights />
           {/* <VersionManager /> */}
         </ConvexProvider>
       </SessionProvider>
